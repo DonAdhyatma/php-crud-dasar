1b5282937a353e79f11167cc2dc0e3e06e004e31
DanniAdhyatma <dannirachman2@gmail.com>
2023-06-13 14:42:10 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+include 'functions.php';
+// Your PHP code here.
+
+// Home Page template below.
+?>
+
+<?=template_header('Home')?>
+
+<div class="content">
+	<h2>Home</h2>
+	<p>Selamat datang di home page data pegawai!</p>
+	<p>Inilah yang dimaksud dengan backend!</p>
+</div>
+
+<?=template_footer()?>
